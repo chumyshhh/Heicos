7beb0253f5461a2db5bac45cd06d9ba6345d96eb
JorikDura <vandarkslave@outlook.com>
2025-01-19 21:52:54 +0300

    removed imports

--- app/src/main/java/com/heicos/presentation/cosplays/CosplaysScreenViewModel.kt
+++ app/src/main/java/com/heicos/presentation/cosplays/CosplaysScreenViewModel.kt
@@ -18,7 +18,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
